43f5de68b64fea7015ac64899339312c70070b3f
Dimitrios Liasis <your_email@example.com>
2024-01-09 14:04:33 +0200

    instal psql driver and draft first code to test connection with the database. Use usename credential as environmental variable

---
+++ cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	// pq is the library that allows Go to interact with PostgreSQL
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432 // Default port for PostgreSQL
+	dbname   = "task_manager" // Your database name
+)
+
+func main() {
+	// Retrieve the username from the environment variable
+	user := os.Getenv("POSTGRES_USER")
+	if user == "" {
+		log.Fatal("The POSTGRES_USER environment variable is not set.")
+	}
+
+	// Construct the connection string
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable", host, port, user, dbname)
+
+	// Open a connection to the database
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Error opening connection: %s", err)
+	}
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error pinging database: %s", err)
+	}
+
+	fmt.Println("Successfully connected to database!")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/DimWebDev/task-manager-tool
 
 go 1.21.5
+
+require github.com/lib/pq v1.10.9 // indirect
